b9c1698a1c74d4cbf7b461017baa4ef1ed898c3a
Srinithi <sri.r982006@gmail.com>
2020-07-23 10:49:54 +0530

    Added picture signs + and - instead of textView

--- app/src/main/java/com/example/mathgame2/RandomGenerate.java
+++ app/src/main/java/com/example/mathgame2/RandomGenerate.java
@@ -1,17 +1,23 @@
 package com.example.mathgame2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.Random;
 
 public class RandomGenerate extends AppCompatActivity {
+
+    //ConstraintLayout cLa
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,16 +29,25 @@ public class RandomGenerate extends AppCompatActivity {
         int chooseNum = randomnumber.nextInt(2-0)+0;
         int negative,positive;
         int solution=0;
-        TextView operand = (TextView)findViewById(R.id.textView2);
+        LinearLayout linear_layout = (LinearLayout)findViewById(R.id.linear_layout);
+
         if(chooseNum == 0){
             negative = randomnumber.nextInt(0+15)-15;
             solution = displayNum-negative;
-            operand.setText("Negative : "+Integer.toString(negative));
+            for(int i=0;i<Math.abs(negative);i++){
+                linear_layout.addView(getImageView(chooseNum));
+            }
+
+            //operand.setText("Negative : "+Integer.toString(negative));
         }
         else{
             positive = randomnumber.nextInt(16-0)+0;
             solution = displayNum-positive;
-            operand.setText("Positive : "+Integer.toString(positive));
+            for(int i=0;i<Math.abs(positive);i++){
+                linear_layout.addView(getImageView(chooseNum));
+            }
+
+            //operand.setText("Positive : "+Integer.toString(positive));
         }
 
         final EditText input = (EditText)findViewById(R.id.editText);
@@ -57,5 +72,24 @@ public class RandomGenerate extends AppCompatActivity {
             result.setText("Better Luck Next Time!!");
         }
 
+    }
+    private View getImageView(int imgSelect){
+        ImageView iv = new ImageView(getApplicationContext());
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(20,1, 10,10);
+        iv.setLayoutParams(lp);
+        iv.requestLayout();
+        iv.getLayoutParams().height=30;
+        iv.getLayoutParams().width=30;
+        if(imgSelect==0){
+            iv.setImageResource(R.drawable.minus);
+        }
+        else{
+            iv.setImageResource(R.drawable.plus);
+        }
+
+
+        //iv.setImageResource(R.drawable.minus);
+        return iv;
     }
 }
